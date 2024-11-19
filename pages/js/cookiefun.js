2afb396c0d05a867ccff27d25d5a42fb334f60ec
Rudradip Ray <317295@aaps.k12.mi.us>
2024-11-18 22:47:29 -0500

    partial json setup

---
+++ pages/js/cookiefun.js
@@ -0,0 +1,19 @@
+```
+type: TICKET or ITEM
+name: NAME
+loc: LOCATION or None
+quant: QUANTITY
+```
+
+
+function addCookie (type, name, loc, quant) {
+
+}
+
+function deleteCookie () {
+
+}
+
+function changeQuant (type, name, quant) {
+
+}
