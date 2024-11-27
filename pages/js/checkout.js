d90570a95ad659652f736711f0b1f8e584cd8f88
Rudradip Ray <317295@aaps.k12.mi.us>
2024-11-27 01:11:38 -0500

    Checkout ITEM insert implemented

--- pages/js/checkout.js
+++ pages/js/checkout.js
@@ -15,10 +15,29 @@
     })
   })()
 
-  async function insertItems () {
-    //const ins = document.getElementById("insert-items");
-    var keyValuePairs = document.cookie
-    console.log(keyValuePairs)
+  async function insertStuffs () {
+    const ins = document.getElementById("insert-items");
+    var keyValuePairs = document.cookie.split(";");
+    for (var i=0;i<keyValuePairs.length;i++) {
+      console.log("hello");
+      if (keyValuePairs[i].split("=")[0].trim() == "items") {
+        console.log("turkey");
+        arrS = keyValuePairs[i].split("=")[1];
+        console.log(arrS);
+        arr = arrS.split(",");
+        for (var j=0;j<arr.length;j++) {
+          ii = arr[j]
+          sep = ii.split("/");
+          let item = document.createElement('checkout-item-c');
+          item.setAttribute("name", sep[0]);
+          item.setAttribute("extra", sep[1]);
+          item.setAttribute("qty", sep[2]);
+          item.setAttribute("price", sep[3]);
+          ins.insertAdjacentElement("afterend",item);
+        }
+        console.log(arrS);
+      }
+    }
   }
 
-insertItems()
+insertStuffs ()
