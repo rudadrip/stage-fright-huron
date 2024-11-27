9482c18b761923d176097a78c773e8f4408cda86
Rudradip Ray <317295@aaps.k12.mi.us>
2024-11-27 01:16:49 -0500

    Checkout CONCERT insert implemented

--- pages/js/checkout.js
+++ pages/js/checkout.js
@@ -21,7 +21,6 @@
     for (var i=0;i<keyValuePairs.length;i++) {
       console.log("hello");
       if (keyValuePairs[i].split("=")[0].trim() == "items") {
-        console.log("turkey");
         arrS = keyValuePairs[i].split("=")[1];
         console.log(arrS);
         arr = arrS.split(",");
@@ -29,10 +28,25 @@
           ii = arr[j]
           sep = ii.split("/");
           let item = document.createElement('checkout-item-c');
-          item.setAttribute("name", sep[0]);
-          item.setAttribute("extra", sep[1]);
-          item.setAttribute("qty", sep[2]);
-          item.setAttribute("price", sep[3]);
+          item.setAttribute("name", sep[0].trim());
+          item.setAttribute("extra", sep[1].trim());
+          item.setAttribute("qty", sep[2].trim());
+          item.setAttribute("price", sep[3].trim());
+          ins.insertAdjacentElement("afterend",item);
+        }
+        console.log(arrS);
+      } else if (keyValuePairs[i].split("=")[0].trim() == "concerts") {
+        arrS = keyValuePairs[i].split("=")[1];
+        console.log(arrS);
+        arr = arrS.split(",");
+        for (var j=0;j<arr.length;j++) {
+          ii = arr[j]
+          sep = ii.split("/");
+          let item = document.createElement('checkout-item-c');
+          item.setAttribute("name", sep[0].trim());
+          item.setAttribute("extra", sep[1].trim());
+          item.setAttribute("qty", sep[2].trim());
+          item.setAttribute("price", sep[3].trim());
           ins.insertAdjacentElement("afterend",item);
         }
         console.log(arrS);
