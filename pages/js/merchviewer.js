18db5e41d764f3376d9e4b8c4b3b85bf9cb5ef26
Yeetmun <55354915+Yeetmun@users.noreply.github.com>
2024-11-29 20:59:29 -0500

    connected merch and merch viewer

--- pages/js/merchviewer.js
+++ pages/js/merchviewer.js
@@ -1,11 +1,12 @@
-function to_merch_view(event){;
+function to_merch_view(event){
+    document.cookie = `current_merch_item=${event.srcElement.parentNode.getElementsByTagName("h5")[0].innerHTML}`;
     window.location.href = "/pages/merchviewer.html";
 }
 
 async function display_venue(){
     let data = await fetch("/pages/veryrealdatabase/merch.json");
-    const all_mech = await data.json();
-    //console.log(all_locations); 
+    const all_merch = await data.json();
+    //console.log(all_merch); 
     let raw_cookies = document.cookie;
     raw_cookies = raw_cookies.toString().split("; ");
     const cookies = {};
@@ -14,9 +15,12 @@ async function display_venue(){
         cookies[current[0]] = current[1];
     }
 
-    for (let i = 0; i < all_locations.length; i++){
-        if (all_locations[i].loc === cookies.venue){
-            //Edit elements by id here
+    for (let i = 0; i < all_merch.length; i++){
+        if (all_merch[i].name === cookies.current_merch_item){
+            document.getElementById("title").innerHTML = all_merch[i].name;
+            document.getElementById("merch-details").innerHTML = all_merch[i].desc;
+            document.getElementById("merch-image").src = all_merch[i].image;
+            document.getElementById("price").innerHTML = `$${all_merch[i].price}`;
             return;
         }
     }
